c547c224ae765fe3bfffd8fb5a057d1c91b24043
jagdeep jindal <jagdeepjindal007@gmail.com>
2026-05-05 13:12:05 +0530

    icons addd

--- src/components/screens/BottomTabs.tsx
+++ src/components/screens/BottomTabs.tsx
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from './tabs/HomeScreen';
 import { ProfileScreen } from './tabs/ProfileScreen';
 import { Button } from '@react-navigation/elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+declare module 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,7 +16,27 @@ export function MyTabs() {
             //     backgroundColor:"#000"
             // },
             headerTitleAlign: "center",
-            
+
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName = '';
+                let label = ''
+
+                if (route.name === 'Home') {
+                    iconName = focused
+                        ? 'home'
+                        : 'home-outline';
+                    label = 'homehu'
+                } else if (route.name === 'Profile') {
+                    iconName = focused ? 'user' : 'user-o';
+                    label = 'prof'
+                }
+
+                // You can return any component that you like here!
+                return <Icon name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+
         })} >
             <Tab.Screen name="Home" component={HomeScreen} options={{
                 headerRight: () => (
